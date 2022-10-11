89d56656eab4f1496e671104776a512b5cc4ee7a
SOLANKI <SOLANKI@DESKTOP-IRM81VO>
2022-10-11 16:05:18 +0530

    commit

---
+++ src/SeleniumProgram/DemoTesting.java
@@ -0,0 +1,10 @@
+package SeleniumProgram;
+
+public class DemoTesting 
+{
+@Test
+public void demo void Test()
+{
+	Reporter.log("wel come",true)
+}
+}
---
+++ src/SeleniumProgram/First.java
@@ -0,0 +1,22 @@
+package SeleniumProgram;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class First {
+       static
+	
+	 {
+		 System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+	 }
+	 public static void main(String[]arg)
+	 {
+		 WebDriver driver = new ChromeDriver();
+		 driver.get("https://www.google.com/");
+		
+
+	 }
+
+	}
+
+
